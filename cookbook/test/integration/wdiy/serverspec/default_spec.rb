92a88f84860807fb4da9169eca25f99bb31de7c0
Emily Casey <ecasey@ThoughtWorks.com>
2015-01-22 18:26:03 -0500

    moving to ubuntu 14

--- cookbook/test/integration/wdiy/serverspec/default_spec.rb
+++ cookbook/test/integration/wdiy/serverspec/default_spec.rb
@@ -4,10 +4,6 @@ require 'serverspec'
 set :backend, :exec
 set :path, '/opt/rbenv/bin:/opt/rbenv/shims:$PATH'
 
-RSpec.configure do |c|
-  user    = 'vagrant'
-end
-
 describe command('rbenv global') do
-  its(:stdout) { should match '2.1.1' }
+  its(:stdout) { should match 'system' }
 end
