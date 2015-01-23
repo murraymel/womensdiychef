c566710d3ba74871176bf2f1de603885649aac09
Emily Casey <ecasey@ThoughtWorks.com>
2015-01-23 01:29:32 -0500

    fixing severspec test so it works for both ubuntu and centos

--- cookbook/test/integration/wdiy/serverspec/default_spec.rb
+++ cookbook/test/integration/wdiy/serverspec/default_spec.rb
@@ -2,8 +2,8 @@ require 'serverspec'
 
 # Required by serverspec
 set :backend, :exec
-set :path, '/opt/rbenv/bin:/opt/rbenv/shims:$PATH'
+set :path, '/opt/rbenv/bin:/opt/rbenv/shims:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 
-describe command('rbenv global') do
-  its(:stdout) { should match 'system' }
+describe command('ruby --version') do
+  its(:stdout) { should include '2.1.1' }
 end
